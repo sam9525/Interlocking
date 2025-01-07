843e2a4dfe8beebc0acfdd92436828d927becb9b
sam9525 <sam9985246327@gmail.com>
2025-01-07 14:04:08 +0800

    Create transitions and store in a List

--- src/InterlockingImpl.java
+++ src/InterlockingImpl.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class InterlockingImpl implements Interlocking {
@@ -9,8 +11,12 @@ public class InterlockingImpl implements Interlocking {
   // Map of track sections to their current number of tokens
   private Map<Integer, TrackSections> trackSections = new HashMap<>();
 
+  // List of transitions
+  private List<Transitions> transitions = new ArrayList<>();
+
   public InterlockingImpl() {
     initializeTrackSections();
+    initializeTransitions();
   }
 
   /**
@@ -37,11 +43,45 @@ public class InterlockingImpl implements Interlocking {
       70,
     };
 
+    // Add track sections to the map
     for (int id : sectionIds) {
       trackSections.put(
         id,
         new TrackSections(id, id == 50 || id == 60 || id == 70 ? 1 : 0)
       );
+    }
+  }
+
+  /**
+   * Initialize the transitions
+   */
+  private void initializeTransitions() {
+    String[] transitionIds = {
+      "ts15",
+      "ts59",
+      "ts51",
+      "ts95",
+      "ts26",
+      "ts610",
+      "ts106",
+      "ts62",
+      "ts58",
+      "ts85",
+      "ts69",
+      "ts96",
+      "ts68",
+      "ts86",
+      "ts37",
+      "ts711",
+      "ts117",
+      "ts73",
+      "ts34",
+      "ts43",
+    };
+
+    // Add transitions to the list
+    for (String id : transitionIds) {
+      transitions.add(new Transitions(id));
     }
   }
 }
