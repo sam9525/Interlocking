f700406dea8cb1baba2315ca1ddb44b06abb5355
sam9525 <sam9985246327@gmail.com>
2025-01-07 13:57:35 +0800

    Create the track sections and store in a map

--- src/InterlockingImpl.java
+++ src/InterlockingImpl.java
@@ -5,4 +5,43 @@ public class InterlockingImpl implements Interlocking {
 
   // Map of trains to their current track section
   private Map<String, Trains> trains = new HashMap<>();
+
+  // Map of track sections to their current number of tokens
+  private Map<Integer, TrackSections> trackSections = new HashMap<>();
+
+  public InterlockingImpl() {
+    initializeTrackSections();
+  }
+
+  /**
+   * Initialize the track sections
+   */
+  private void initializeTrackSections() {
+    int[] sectionIds = {
+      1,
+      5,
+      9,
+      2,
+      6,
+      10,
+      8,
+      3,
+      7,
+      11,
+      4,
+      51,
+      50,
+      61,
+      60,
+      71,
+      70,
+    };
+
+    for (int id : sectionIds) {
+      trackSections.put(
+        id,
+        new TrackSections(id, id == 50 || id == 60 || id == 70 ? 1 : 0)
+      );
+    }
+  }
 }
